8216c4025aabc3590922cca6a9fc509a36626f9f
duncanproctor <duncpro@icloud.com>
2024-08-28 19:03:19 -0400

    create directories if they do not already exist

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -14,15 +14,16 @@ pub struct Cli {
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
-    let mut input_path = std::env::current_dir().unwrap();
+    let mut input_path = std::env::current_dir()?;
     input_path.push(&cli.input_path);
-    
+  
     if input_path.is_dir() {
         compile_dir(input_path, cli.output_path, &cli.head)?;
         return Ok(());
     }
 
     if input_path.is_file() {
+        std::fs::create_dir_all(cli.output_path.parent().unwrap())?;
         compile_file(input_path, cli.output_path, &cli.head)?;
         return Ok(());
     }
